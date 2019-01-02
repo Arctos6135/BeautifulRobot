863094a4d819ec016cc98ade7c108051eb2ee568
Tyler Tian <tylertian123@gmail.com>
2019-01-01 23:30:44 -0500

    Re-add interrupts

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@ typedef unsigned short uint16_t;
 //Param: an integer in the range 0 - 3, the team color. 0 - red, 1 - blue, 2 - green. Default: 2.
 #define CMD_COLOR 		0x04
 
-#define LED_COUNT 30
+#define LED_COUNT 60
 
 uint8_t brightness = 100;
 bit dispOn = 0;
@@ -54,7 +54,7 @@ void processCmd(uint16_t cmdBuf) {
 	}
 }
 
-xdata RGBColor colors[LED_COUNT] = { 0 };
+xdata volatile RGBColor colors[LED_COUNT] = { 0 };
 void Timer0Init(void) { //30ms@12.000MHz
 	//Enable interrupts
 	EA = 1;
@@ -67,24 +67,24 @@ void Timer0Init(void) { //30ms@12.000MHz
 	TF0 = 0;		//Clear TF0 flag
 	TR0 = 1;		//Timer0 start run
 }
-//void Timer0Routine(void) interrupt 1 {
-//	unsigned char i;
-//	//Clear flag
-//	TF0 = 0;
-//	//Reset timer
-//	TL0 = 0xD0;
-//	TH0 = 0x8A;
-//	
-//	if(dispOn) {
-//		LED_SendRGBData(colors, LED_COUNT);
-//	}
-//	else {
-//		for(i = 0; i < LED_COUNT; i ++) {
-//			LED_SendColor(0, 0, 0);
-//		}
-//		LED_Latch();
-//	}
-//}
+void Timer0Routine(void) interrupt 1 {
+	unsigned char i;
+	//Reset timer
+	TL0 = 0xD0;
+	TH0 = 0x8A;
+	//Clear flag
+	TF0 = 0;
+	
+	if(dispOn) {
+		LED_SendRGBData(colors, LED_COUNT);
+	}
+	else {
+		for(i = 0; i < LED_COUNT; i ++) {
+			LED_SendColor(0, 0, 0);
+		}
+		LED_Latch();
+	}
+}
 
 void generateColors(void) {
 	unsigned char i;
@@ -111,21 +111,11 @@ int main(void) {
 	}
 	LED_Latch();
 	
-	//Timer0Init();
-	
-	generateColors();
+	Timer0Init();
 	
 	while(1) {
-		if(dispOn) {
-			LED_SendRGBData(colors, LED_COUNT);
-		}
-		else {
-			for(i = 0; i < LED_COUNT; i ++) {
-				LED_SendColor(0, 0, 0);
-			}
-			LED_Latch();
-		}
 		
+		generateColors();
 		
 		//When UART_Buffer > 0xFF, at least one bit in the first byte must be 1. Therefore 2 bytes have been received.
 		if(UART_Buffer > 0xFF) {
