7a035ac5ebcc86078bf898a3c0a6b01dad7fea63
Tyler Tian <tylertian123@gmail.com>
2019-01-06 22:43:24 -0500

    Improve FRC Java example
    Modify examples/BeautifulRobot.java so now the checked writes will try
    again if the BeautifulRobot failed to respond within 100ms. Also add
    methods that allow concurrent command writing.

--- examples/BeautifulRobot.java
+++ examples/BeautifulRobot.java
@@ -4,17 +4,25 @@
 
 package frc.robot.components;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import edu.wpi.first.wpilibj.SerialPort;
 
 /**
  * <p>
  * The BeautifulRobot&#8482; is an 8051-based WS2812B RGB LED strip controller, interfaced with UART.
+ * This class provides methods to write commands to the BeautifulRobot&#8482;, write commands with error checking,
+ * and write commands concurrently (since each write takes at least 150ms).
  * </p>
  * For more information, refer to the README in its repository on <a href="https://github.com/Arctos6135/BeautifulRobot#beautifulrobot">GitHub</a>.
  */
 public class BeautifulRobot {
     
-    protected SerialPort serial;
+    protected final SerialPort serial;
+    //A single-threaded ExecutorService is used for concurrent writes
+    protected final ExecutorService writeExecutor;
 
     /**
      * <p>
@@ -45,6 +53,7 @@ public class BeautifulRobot {
      * @param port The serial port it's connected to
      */
     public BeautifulRobot(SerialPort.Port port) {
+        writeExecutor = Executors.newSingleThreadExecutor();
         //Should default to 8 data bits, one stop bit, and no parity bit
         serial = new SerialPort(9600, port);
 
@@ -56,9 +65,12 @@ public class BeautifulRobot {
     }
 
     /**
-     * Writes a command to the BeautifulRobot&#8482;. This method takes slightly more than 150ms to execute.
+     * Writes a command to the BeautifulRobot&#8482;.
      * There is no error checking to make sure the right values were received. Use {@link #writeCommandWithCheck(Operation, byte, int)}
      * instead.
+     * <p>
+     * This method will block for at least 150ms to execute. Consider using {@link #concurrentWrite(Operation, byte)} instead.
+     * </p>
      * @param op The operation
      * @param param The parameter
      */
@@ -67,36 +79,41 @@ public class BeautifulRobot {
         byte[] opArr = new byte[] { op.getCode() };
         byte[] paramArr = new byte[] { param };
         byte[] sync = new byte[] { (byte) 0xFF };
-
-        try {
-            //Write each one and delay
-            serial.write(opArr, 1);
-            Thread.sleep(50);
-            serial.write(paramArr, 1);
-            Thread.sleep(50);
-            serial.write(sync, 1);
-            Thread.sleep(50);
-        }
-        catch(InterruptedException e) {
-            e.printStackTrace();
+        
+        synchronized(this) {
+            try {
+                //Write each one and delay
+                serial.write(opArr, 1);
+                Thread.sleep(50);
+                serial.write(paramArr, 1);
+                Thread.sleep(50);
+                serial.write(sync, 1);
+                Thread.sleep(50);
+            }
+            catch(InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     /**
      * <p>
-     * Writes a command to the BeautifulRobot&#8482;. Each write operation takes slightly more than 150ms.
+     * Writes a command to the BeautifulRobot&#8482;.
      * This method performs error checking to make sure the right values were received by the BeautifulRobot&#8482;.
      * </p>
      * <p>
-     * If the BeautifulRobot&#8482; takes longer than 100ms to respond, this method times out and returns {@code false}.
-     * If the value received by the BeautifulRobot&#8482; was not correct, this method will try again, until a total of
-     * 2 attempts has been reached.
+     * If the value received by the BeautifulRobot&#8482; was not correct, or if the BeautifulRobot&#8482; fails to
+     * respond within 100ms, this method will try again, until a total of 2 attempts have failed.
+     * </p>
+     * <p>
+     * This method will block for at least 150ms to execute, and maybe even more depending on the number of failed
+     * attempts. Consider using {@link #concurrentWriteWithCheck(Operation, byte)} instead.
      * </p>
      * @param op The operation
      * @param param The parameter
-     * @return {@code true} if write successful, {@code false} if timed out or maximum number of attempts reached.
+     * @return {@code true} if write successful, {@code false} if the maximum number of attempts was reached.
      */
-    public boolean writeCommandWithCheck(Operation op, byte param) {
+    public synchronized boolean writeCommandWithCheck(Operation op, byte param) {
         return writeCommandWithCheck(op, param, 2);
     }
     /**
@@ -105,16 +122,19 @@ public class BeautifulRobot {
      * This method performs error checking to make sure the right values were received by the BeautifulRobot&#8482;.
      * </p>
      * <p>
-     * If the BeautifulRobot&#8482; takes longer than 100ms to respond, this method times out and returns {@code false}.
-     * If the value received by the BeautifulRobot&#8482; was not correct, this method will try again, until the specified
-     * number of attempts has been reached.
+     * If the value received by the BeautifulRobot&#8482; was not correct, or if the BeautifulRobot&#8482; fails to
+     * respond within 100ms, this method will try again, until the specified number of attempts has been reached.
+     * </p>
+     * <p>
+     * This method will block for at least 150ms to execute, and maybe even more depending on the number of failed
+     * attempts. Consider using {@link #concurrentWriteWithCheck(Operation, byte, int)} instead.
      * </p>
      * @param op The operation
      * @param param The parameter
      * @param maxAttempts The maximum number of attempts to make before giving up
-     * @return {@code true} if write successful, {@code false} if timed out or maximum number of attempts reached.
+     * @return {@code true} if write successful, {@code false} if the maximum number of attempts was reached.
      */
-    public boolean writeCommandWithCheck(Operation op, byte param, int maxAttempts) {
+    public synchronized boolean writeCommandWithCheck(Operation op, byte param, int maxAttempts) {
         //Keep track of the starting time
         long start;
 
@@ -127,17 +147,22 @@ public class BeautifulRobot {
 
             start = System.currentTimeMillis();
             //Wait for echo
+            boolean echoReceived = true;
             while(serial.getBytesReceived() < 2) {
                 //Time out after 100ms
                 if(System.currentTimeMillis() - start >= 100) {
-                    return false;
+                    echoReceived = false;
+                    break;
                 }
             }
-            byte[] echo = serial.read(2);
-            
-            //Verify that the echo is the same as the bytes we sent
-            if(echo[0] == op.getCode() && echo[1] == param) {
-                return true;
+            //Verify echo if received
+            if(echoReceived) {
+                byte[] echo = serial.read(2);
+                
+                //Verify that the echo is the same as the bytes we sent
+                if(echo[0] == op.getCode() && echo[1] == param) {
+                    return true;
+                }
             }
             //Otherwise check if we exceeded the max number of attempts
             --maxAttempts;
@@ -145,5 +170,58 @@ public class BeautifulRobot {
                 return false;
             }
         }
+    }
+
+    /**
+     * Concurrently writes a command to the BeautifulRobot&#8482;. The write takes at least 150ms, but this method does not block.
+     * There is no error checking to make sure the right values were received. Use {@link #concurrentWriteWithCheck(Operation, byte, int)}
+     * instead.
+     * @param op The operation
+     * @param param The parameter
+     * @return A {@code Future} representing pending completion of the write
+     */
+    public Future<?> concurrentWrite(Operation op, byte param) {
+        return writeExecutor.submit(() -> {
+            writeCommand(op, param);
+        });
+    }
+    /**
+     * <p>
+     * Concurrently writes a command to the BeautifulRobot&#8482;. The write takes at least 150ms, but this method does not block.
+     * This method performs error checking to make sure the right values were received by the BeautifulRobot&#8482;.
+     * </p>
+     * <p>
+     * If the value received by the BeautifulRobot&#8482; was not correct, or if the BeautifulRobot&#8482; fails to
+     * respond within 100ms, this method will try again, until a total of 2 attempts have failed.
+     * </p>
+     * @param op The operation
+     * @param param The parameter
+     * @return A {@code Future} representing pending completion of the write. The result is a true if the write was successful,
+     * false otherwise.
+     */
+    public Future<Boolean> concurrentWriteWithCheck(Operation op, byte param) {
+        return writeExecutor.submit(() -> {
+            return writeCommandWithCheck(op, param);
+        });
+    }
+    /**
+     * <p>
+     * Writes a command to the BeautifulRobot&#8482;. The write takes at least 150ms, but this method does not block.
+     * This method performs error checking to make sure the right values were received by the BeautifulRobot&#8482;.
+     * </p>
+     * <p>
+     * If the value received by the BeautifulRobot&#8482; was not correct, or if the BeautifulRobot&#8482; fails to
+     * respond within 100ms, this method will try again, until the specified number of attempts has been reached.
+     * </p>
+     * @param op The operation
+     * @param param The parameter
+     * @param maxAttempts The maximum number of attempts to make before giving up
+     * @return A {@code Future} representing pending completion of the write. The result is a true if the write was successful,
+     * false otherwise.
+     */
+    public Future<Boolean> concurrentWriteWithCheck(Operation op, byte param, int maxAttempts) {
+        return writeExecutor.submit(() -> {
+            return writeCommandWithCheck(op, param, maxAttempts);
+        });
     }
 }
